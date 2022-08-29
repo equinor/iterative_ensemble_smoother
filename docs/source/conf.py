78e72fbedf34c6e62209c88f05895de926bf20d9
Eivind Jahren <ejah@equinor.com>
2022-08-29 09:06:17 +0200

    Update notebooks in docs
    * Sets up building of ipynb from py automatically when building docs
    * More documentation of the notebooks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,8 +1,14 @@
+from subprocess import check_output
+
 project = "iterative_ensemble_smoother"
 copyright = "2022, Equinor"
 author = "Equinor"
 release = "0.0.1"
 
+
+check_output(["jupytext", "Polynomial.py", "-o", "Polynomial.ipynb"])
+
+check_output(["jupytext", "Oscillator.py", "-o", "Oscillator.ipynb"])
 
 extensions = [
     "sphinx.ext.autodoc",
