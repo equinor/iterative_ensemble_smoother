04b81868c0cbf571e46de7cf5abf006c4bb43999
Feda Curic <feda.curic@gmail.com>
2023-01-04 11:58:44 +0100

    Move reference to paper to __init__.py

--- src/iterative_ensemble_smoother/__init__.py
+++ src/iterative_ensemble_smoother/__init__.py
@@ -1,8 +1,9 @@
 """ Implementation of the iterative ensemble smoother history matching algorithms
-from Evensen, G. "Analysis of iterative ensemble smoothers for solving inverse
-problems." for details about the algorithm.
-
+from Evensen et al. "Efficient Implementation of an Iterative Ensemble Smoother 
+for Data Assimilation and Reservoir History Matching"
+https://www.frontiersin.org/articles/10.3389/fams.2019.00047/full
 """
+
 from ._ensemble_smoother import ensemble_smoother_update_step
 from ._ies import InversionType
 from ._iterative_ensemble_smoother import IterativeEnsembleSmoother
