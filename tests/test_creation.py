d4764f4a31203646818be2cd2ec68a972782a590
tommyod <tommy.odland@gmail.com>
2023-06-30 15:26:39 +0200

    removed script folder

--- tests/test_creation.py
+++ tests/test_creation.py
@@ -76,23 +76,31 @@ def test_that_nans_produced_due_to_outliers_in_responses_are_handled():
     # Creating response matrix with large outlier that will
     # lead to NaNs.
 
+    obs_error = np.array([1, 2])
+    obs_value = np.array([10, 20])
+
     with pytest.raises(
         ValueError,
         match="Fit produces NaNs. Check your response matrix for outliers or use an inversion type with truncation.",
     ):
 
+        failed = False
         # Keep going until error is raised
         for exponent in np.linspace(10, 200, num=25):
-            response_ensemble = np.array([[1, 0], [1, 10**exponent]], dtype=float)
-            obs_error = np.array([1, 2])
-            obs_value = np.array([10, 20])
-            smoother = ES()
-            smoother.fit(response_ensemble, obs_error, obs_value)
 
+            if failed:
+                break
+
+            response_ensemble = np.array([[1, 0], [1, 10**exponent]], dtype=float)
+            smoother = ES()
+            try:
+                smoother.fit(response_ensemble, obs_error, obs_value)
+            except Exception as e:
+                failed = True
+                raise e
+
+    # This one failed
     response_ensemble = np.array([[1, 0], [1, 10**exponent]], dtype=float)
-    obs_error = np.array([1, 2])
-    obs_value = np.array([10, 20])
-    smoother = ES()
 
     # Running with an inversion type that does truncation does not produce NaNs.
     param_ensemble = np.array([[1, 2]])
