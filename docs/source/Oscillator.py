0794d8facf8bf54d6496f000947ac3aa53fa9265
tommyod <tommy.odland@gmail.com>
2023-09-01 13:13:53 +0200

    updated example notebooks

--- docs/source/Oscillator.py
+++ docs/source/Oscillator.py
@@ -17,23 +17,69 @@
 # # Example: Estimating parameters of an anharmonic oscillator
 #
 # The anharnomic oscillator can be modelled by a non-linear partial differential
-# equation as described in section 6.4.3 of the book Fundamentals of Algorithms
-# and Data Assimilation by Mark Asch, Marc Bocquet and Maëlle Nodet.
+# equation as described in section 6.3.4 of the book [Fundamentals of Algorithms
+# and Data Assimilation](https://www.amazon.com/Data-Assimilation-Methods-Algorithms-Applications/dp/1611974534) by Mark Asch, Marc Bocquet and Maëlle Nodet.
+#
+# -------------
+#
+# The discrete two-parameter model is:
+#
+# $$x_{k+1} - 2 x_{k} + x_{k-1} = \omega^2 x_{k} - \lambda^2 x_{k}^3$$
+#
+# with initial conditions $x_0 = 0$ and $x_1 = 1$.
+#
+# In other words we have the following recurrence relationship:
+#
+# $x_{k+1} = \omega^2 x_{k} - \lambda^2 x_{k}^3 + 2 x_{k} - x_{k-1}$
+#
+#
+# -------------
+# The state vector can be written as:
+#
+# $$
+# \mathbf{u}_k = \begin{bmatrix}
+# x_{k} \\
+# x_{k-1}
+# \end{bmatrix}
+# \quad
+# \mathcal{M}_{k+1} =
+#  \begin{bmatrix}
+# 2 + \omega^2 - \lambda^2 x_k^2 & -1 \\
+# 1 & 0
+# \end{bmatrix}
+# $$
+#
+# so that $\mathbf{u}_{k+1} = \mathcal{M}_{k+1}(\mathbf{u}_k)$.
+#
+#
 
 # %%
-# Simple plotting of forward-model with a single response and parameters
 from matplotlib import pyplot as plt
+import numpy as np
+from scipy.special import erf
+
+rng = np.random.default_rng(12345)
+
+plt.rcParams["figure.figsize"] = [8, 3]
 
 
-def plot_result(A, response_x_axis, trans_func=lambda x: x, priors=[]):
-    responses = forward_model(A, priors, response_x_axis)
-    plt.rcParams["figure.figsize"] = [15, 4]
-    _, axs = plt.subplots(1, 1 + len(A))
+# %%
+def plot_result(A, response_x_axis):
+    """Plot the anharmonic oscillator, as well as marginal
+    and joint distributions for the parameters."""
 
-    axs[0].plot(response_x_axis, responses)
-    for i, param in enumerate(A):
-        A_trans = np.array([trans_func(v, *priors[i]) for v in param])
-        axs[i + 1].hist(A_trans, bins=10)
+    responses = forward_model(A, response_x_axis)
+
+    fig, axes = plt.subplots(1, 2 + len(A), figsize=(9, 2.25))
+
+    axes[0].plot(response_x_axis, responses, color="black", alpha=0.1)
+    for ax, param, label in zip(axes[1:], A, ["omega", "lambda"]):
+        ax.hist(param, bins="fd")
+        ax.set_xlabel(label)
+
+    axes[-1].scatter(A[0, :], A[1, :], s=5)
+
+    fig.tight_layout()
     plt.show()
 
 
@@ -41,74 +87,77 @@ def plot_result(A, response_x_axis, trans_func=lambda x: x, priors=[]):
 # ## Setup
 
 # %%
-# Oscilator example
-import numpy as np
-from math import sqrt
-from scipy.special import erf
-
-
 def _generate_observations(K):
-    x = _evaluate(omega=3.5e-2, lmbda=3e-4, K=K)
-    rng = np.random.default_rng(12345)
+    """Run the model with true parameters, then generate observations."""
+    # Evaluate using true parameter values on a fine grid with K sample points
+    x = simulate_anharmonic(omega=3.5e-2, lmbda=3e-4, K=K)
+
+    # Generate observations every `nobs` steps
     nobs = 50
-    obs_points = np.linspace(0, K, nobs, endpoint=False, dtype=int)
+    # Generate observation points [0, 50, 100, 150, ...] when `nobs` = 50
+    observation_x_axis = np.arange(K // nobs) * nobs
 
-    obs_with_std = np.zeros(shape=(len(obs_points), 2))
+    # Generate noisy observations, with value at least 5
+    observation_errors = np.maximum(5, 0.2 * np.abs(x[observation_x_axis]))
+    observation_values = rng.normal(loc=x[observation_x_axis], scale=observation_errors)
 
-    for obs_idx, obs_point in enumerate(obs_points):
-        # Set observation error's standard deviation to some
-        # percentage of the amplitude of x with a minimum of, e.g., 1.
-        obs_std = max(1, 0.02 * abs(x[obs_point]))
-        obs_with_std[obs_idx, 0] = x[obs_point] + rng.normal(loc=0.0, scale=obs_std)
-        obs_with_std[obs_idx, 1] = obs_std
-    return obs_with_std, obs_points
+    return observation_values, observation_errors, observation_x_axis
 
 
-def _evaluate(omega, lmbda, K):
+def simulate_anharmonic(omega, lmbda, K):
+    """Evaluate the anharmonic oscillator."""
     x = np.zeros(K)
     x[0] = 0
     x[1] = 1
 
     # Looping from 2 because we have initial conditions at k=0 and k=1.
-    for k in range(2, K - 1):
-        M = np.array([[2 + omega**2 - lmbda**2 * x[k] ** 2, -1], [1, 0]])
-        u = np.array([x[k], x[k - 1]])
-        u = M @ u
-        x[k + 1] = u[0]
-        x[k] = u[1]
+    for k in range(2, K):
+        x[k] = x[k - 1] * (2 + omega**2 - lmbda**2 * x[k - 1] ** 2) - x[k - 2]
 
     return x
 
 
-def uniform(x, min_x, max_x):
-    y = 0.5 * (1 + erf(x / sqrt(2.0)))
-    return y * (max_x - min_x) + min_x
-
-
-def forward_model(A, prior, response_x_axis):
-    responses = []
-    for [omega, lmbda] in A.T:
-        r = _evaluate(
-            omega=uniform(omega, *prior[0]),
-            lmbda=uniform(lmbda, *prior[1]),
-            K=len(response_x_axis),
-        )
-        responses.append(r)
-    return np.array(responses).T
+def forward_model(A, response_x_axis):
+    """Evaluate on each column (ensemble realization)."""
+    return np.vstack(
+        [simulate_anharmonic(*params, K=len(response_x_axis)) for params in A.T]
+    ).T
 
 
 response_x_axis = range(2500)
+observation_values, observation_errors, observation_x_axis = _generate_observations(
+    len(response_x_axis)
+)
+
+
+# %% [markdown]
+# ## Plot observations
+
+# %%
+fig, ax = plt.subplots(figsize=(8, 3))
+
+ax.plot(
+    np.arange(2500),
+    simulate_anharmonic(omega=3.5e-2, lmbda=3e-4, K=2500),
+    label="Truth",
+    color="black",
+)
+
+ax.scatter(observation_x_axis, observation_values, label="Observations")
+
+fig.tight_layout()
+plt.legend()
+plt.show()
+
+# %% [markdown]
+# ## Plot prior
+
+# %%
 realizations = 100
-
-observations, observation_x_axis = _generate_observations(len(response_x_axis))
-observation_values = observations[:, 0]
-observation_errors = observations[:, 1]
-
-A = np.random.normal(0, 1, size=(2, realizations))
-
 priors = [(2.5e-2, 4.5e-2), (2.0e-4, 4.0e-4)]
-plot_result(A, response_x_axis, uniform, priors)
+A = np.vstack([rng.uniform(low, high, size=realizations) for (low, high) in priors])
 
+plot_result(A, response_x_axis)
 
 # %% [markdown]
 # ## Update step
@@ -117,16 +166,16 @@ plot_result(A, response_x_axis, uniform, priors)
 import numpy as np
 import iterative_ensemble_smoother as ies
 
-plot_result(A, response_x_axis, uniform, priors)
+plot_result(A, response_x_axis)
 
-responses_before = forward_model(A, priors, response_x_axis)
+responses_before = forward_model(A, response_x_axis)
 Y = responses_before[observation_x_axis]
 
 smoother = ies.ES()
 smoother.fit(Y, observation_errors, observation_values)
 new_A = smoother.update(A)
 
-plot_result(new_A, response_x_axis, uniform, priors)
+plot_result(new_A, response_x_axis)
 
 # %% [markdown]
 # ## Iterative smoother
@@ -142,14 +191,15 @@ def iterative_smoother():
     smoother = ies.SIES(seed=42)
 
     for _ in range(iterations):
-        plot_result(A_current, response_x_axis, uniform, priors)
+        plot_result(A_current, response_x_axis)
 
-        responses_before = forward_model(A_current, priors, response_x_axis)
+        responses_before = forward_model(A_current, response_x_axis)
         Y = responses_before[observation_x_axis]
 
         smoother.fit(Y, observation_errors, observation_values)
         A_current = smoother.update(A_current)
-    plot_result(A_current, response_x_axis, uniform, priors)
+
+    plot_result(A_current, response_x_axis)
 
 
 iterative_smoother()
@@ -169,17 +219,15 @@ def es_mda():
 
     smoother = ies.ES()
     for weight in weights:
-        plot_result(A_current, response_x_axis, uniform, priors)
+        plot_result(A_current, response_x_axis)
 
-        responses_before = forward_model(A_current, priors, response_x_axis)
+        responses_before = forward_model(A_current, response_x_axis)
         Y = responses_before[observation_x_axis]
 
-        observation_errors_scaled = observation_errors * sqrt(weight * length)
+        observation_errors_scaled = observation_errors * np.sqrt(weight * length)
         smoother.fit(Y, observation_errors_scaled, observation_values)
         A_current = smoother.update(A_current)
-    plot_result(A_current, response_x_axis, uniform, priors)
+    plot_result(A_current, response_x_axis)
 
 
 es_mda()
-
-# %%
--- docs/source/Polynomial.py
+++ docs/source/Polynomial.py
@@ -21,11 +21,14 @@
 # %%
 import numpy as np
 import pandas as pd
+import itertools
 
 np.set_printoptions(suppress=True)
 rng = np.random.default_rng(12345)
 
 import matplotlib.pyplot as plt
+
+COLORS = list(plt.rcParams["axes.prop_cycle"].by_key()["color"])
 
 plt.rcParams["figure.figsize"] = (6, 6)
 plt.rcParams.update({"font.size": 10})
@@ -52,7 +55,7 @@ a_t = 0.5
 b_t = 1.0
 c_t = 3.0
 
-noise_scale = 0.01
+noise_scale = 0.1
 x_observations = [0, 2, 4, 6, 8]
 observations = [
     (
@@ -67,14 +70,16 @@ d = pd.DataFrame(observations, columns=["value", "sd", "x"])
 d = d.set_index("x")
 num_obs = d.shape[0]
 
-fig, ax = plt.subplots()
-x_plot = np.linspace(0, 10, 50)
+fig, ax = plt.subplots(figsize=(7, 4))
+x_plot = np.linspace(0, 10, 2**8)
 ax.set_title("Truth and noisy observations")
 ax.set_xlabel("Time step")
 ax.set_ylabel("Response")
 ax.plot(x_plot, poly(a_t, b_t, c_t, x_plot))
 ax.plot(d.index.get_level_values("x"), d.value.values, "o")
 ax.grid()
+fig.tight_layout()
+plt.show()
 
 # %% [markdown]
 # ## Assume diagonal observation error covariance matrix and define perturbed observations
@@ -91,9 +96,9 @@ D = d.value.values.reshape(-1, 1) + E
 # ## Define Gaussian priors
 
 # %%
-coeff_a = rng.normal(0, 1, size=ensemble_size)
-coeff_b = rng.normal(0, 1, size=ensemble_size)
-coeff_c = rng.normal(0, 1, size=ensemble_size)
+coeff_a = rng.standard_normal(size=ensemble_size)
+coeff_b = rng.standard_normal(size=ensemble_size)
+coeff_c = rng.standard_normal(size=ensemble_size)
 
 X = np.vstack([coeff_a, coeff_b, coeff_c])
 
@@ -163,7 +168,7 @@ for i in range(n_ies_iter):
 
 # %%
 def plot_posterior(ax, posterior, method):
-    for i, param in enumerate(["a", "b", "c"]):
+    for i, param in enumerate("abc"):
         ax[i].set_title(param)
         ax[i].hist(posterior[i, :], label=f"{method} posterior", alpha=0.5, bins="fd")
         ax[i].legend()
@@ -172,8 +177,7 @@ def plot_posterior(ax, posterior, method):
     return ax
 
 
-fig, ax = plt.subplots(nrows=3)
-fig.set_size_inches(8, 8)
+fig, ax = plt.subplots(nrows=3, figsize=(7, 8))
 
 for i in range(3):
     ax[i].hist(X[i, :], label="prior", bins="fd")
@@ -184,3 +188,67 @@ ax[2].axvline(c_t, color="k", linestyle="--", label="truth")
 
 plot_posterior(ax, X_ES_ert, method="ES ert")
 _ = plot_posterior(ax, X_IES_ert, method=f"SIES ert ({n_ies_iter})")
+
+# %%
+fig, axes = plt.subplots(1, 3, figsize=(8, 2.75))
+axes = axes.ravel()
+labels = "abc"
+true_parameters = [a_t, b_t, c_t]
+
+fig.suptitle(f"SIES ert ({n_ies_iter}) Posterior distribution")
+for k, (i, j) in enumerate(itertools.combinations([0, 1, 2], 2)):
+    axes[k].scatter(X[i, :], X[j, :], s=15, alpha=0.6)
+    axes[k].scatter(X_ES_ert[i, :], X_ES_ert[j, :], s=15, alpha=0.2)
+    axes[k].scatter(X_IES_ert[i, :], X_IES_ert[j, :], s=15, alpha=0.2)
+    axes[k].scatter(
+        [true_parameters[i]],
+        [true_parameters[j]],
+        color="black",
+        s=100,
+        label="True value",
+    )
+    axes[k].set_xlabel(labels[i])
+    axes[k].set_ylabel(labels[j])
+
+
+axes[k].legend()
+fig.tight_layout()
+plt.show()
+
+# %%
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(8, 2.5), sharex=True, sharey=True)
+x_plot = np.linspace(0, 10, 2**8)
+
+# Plot the prior
+ax1.set_title("Prior")
+ax1.plot(x_plot, poly(a_t, b_t, c_t, x_plot), zorder=10, lw=4, color="black")
+for parameter_prior in X.T:
+    ax1.plot(
+        x_plot, poly(*parameter_prior, x_plot), color=COLORS[0], alpha=0.1, zorder=5
+    )
+
+# Plot the posterior
+ax2.set_title(f"ES ert posterior")
+ax2.plot(x_plot, poly(a_t, b_t, c_t, x_plot), zorder=10, lw=4, color="black")
+for parameter_posterior in X_ES_ert.T:
+    ax2.plot(
+        x_plot, poly(*parameter_posterior, x_plot), alpha=0.1, zorder=5, color=COLORS[1]
+    )
+
+# Plot the posterior
+ax3.set_title(f"SIES ert ({n_ies_iter}) posterior")
+ax3.plot(x_plot, poly(a_t, b_t, c_t, x_plot), zorder=10, lw=4, color="black")
+for parameter_posterior in X_IES_ert.T:
+    ax3.plot(
+        x_plot, poly(*parameter_posterior, x_plot), alpha=0.1, zorder=5, color=COLORS[2]
+    )
+
+# Common axes setup
+for ax in [ax1, ax2, ax3]:
+    ax.set_ylim([0, 70])
+    ax.set_xlabel("Time step")
+    ax.set_ylabel("Response")
+    ax.grid(zorder=0)
+
+fig.tight_layout()
+plt.show()
