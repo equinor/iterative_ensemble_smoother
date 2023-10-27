f47062ef896b68a7899120e1dae1355420570afa
Tommy Odland <tommy.odland>
2023-10-27 12:36:21 +0200

    RowScaling now mimics C++

--- src/iterative_ensemble_smoother/experimental.py
+++ src/iterative_ensemble_smoother/experimental.py
@@ -11,6 +11,9 @@ from iterative_ensemble_smoother import ESMDA
 
 
 class RowScaling:
+    # Meant to illustrate how row scaling works
+    # For the actual implementation, see
+    # https://github.com/equinor/ert/blob/84aad3c56e0e52be27b9966322d93dbb85024c1c/src/clib/lib/enkf/row_scaling.cpp
     def __init__(self, alpha=1.0):
         """Alpha is the strength of the update."""
         assert 0 <= alpha <= 1.0
@@ -18,8 +21,14 @@ class RowScaling:
 
     def multiply(self, X, K):
         """Takes a matrix X and a matrix K and performs alpha * X @ K."""
+        # This implementation is meant to mimix how RowScaling::assign
+        # behaves in the C++ note. It mutates the input argument X instead of
+        # returning.
+        # https://github.com/equinor/ert/blob/84aad3c56e0e52be27b9966322d93dbb85024c1c/src/clib/lib/enkf/row_scaling.cpp#L74
         # Here K is the smallest matrix, so that's the one we multiply by alpha
-        return X @ (K * self.alpha)
+        assert K.shape[0] == K.shape[1], "Matrix must be square"
+        assert X.shape[1] == K.shape[0], "Dimensions must match"
+        X[:, :] = X @ (K * self.alpha)
 
 
 def ensemble_smoother_update_step_row_scaling(
@@ -57,7 +66,7 @@ def ensemble_smoother_update_step_row_scaling(
             X_i_post = X_i_prior + alpha * X_i_prior @ K
 
     See also original code:
-        https://github.com/equinor/ert/blob/963f9bc08ebc87374b7ed3403c8ba78c20909ae9/src/clib/lib/enkf/row_scaling.cpp#L51
+        https://github.com/equinor/ert/blob/84aad3c56e0e52be27b9966322d93dbb85024c1c/src/clib/lib/enkf/row_scaling.cpp#L51
 
     """
 
@@ -75,9 +84,16 @@ def ensemble_smoother_update_step_row_scaling(
         Y=Y, alpha=1, truncation=truncation
     )
 
+    # The transition matrix K is a matrix such that
+    #     X_posterior = X_prior + X_prior @ K
+    # but the C++ code in ERT requires a transition matrix F that obeys
+    #     X_posterior = X_prior @ K
+    # To accomplish this, we add the identity to the transition matrix in place
+    np.fill_diagonal(transition_matrix, transition_matrix.diagonal() + 1)
+
     # Loop over groups of rows (parameters)
     for X, row_scale in X_with_row_scaling:
-        X += row_scale.multiply(X, transition_matrix)
+        row_scale.multiply(X, transition_matrix)
 
     return X_with_row_scaling
 
@@ -97,7 +113,7 @@ if __name__ == "__main__":
     covariance = np.exp(rng.normal(size=num_observations))
     observations = rng.normal(size=num_observations, loc=1)
 
-    # Split up X into groups of
+    # Split up X into groups of parameters if needed
     row_groups = [(0,), (1, 2), (4, 5, 6), tuple(range(7, 100))]
     X_with_row_scaling = [
         (X[idx, :], RowScaling(alpha=1 / (i + 1))) for i, idx in enumerate(row_groups)
