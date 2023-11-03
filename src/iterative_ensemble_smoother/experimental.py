097f8f8f251e10188d48d4ad6534cc577dc3813d
Tommy Odland <tommy.odland>
2023-11-03 07:11:39 +0100

    some functions

--- src/iterative_ensemble_smoother/experimental.py
+++ src/iterative_ensemble_smoother/experimental.py
@@ -9,6 +9,10 @@ import numpy.typing as npt
 import scipy as sp
 
 from iterative_ensemble_smoother import ESMDA
+from iterative_ensemble_smoother.esmda_inversion import (
+    empirical_covariance_upper,
+    empirical_cross_covariance,
+)
 
 
 def correlation_threshold(ensemble_size: int) -> float:
@@ -22,11 +26,62 @@ def correlation_threshold(ensemble_size: int) -> float:
     return 0.33
 
 
+def compute_cross_covariance_multiplier(
+    *,
+    alpha: float,
+    C_D: npt.NDArray[np.double],
+    D: npt.NDArray[np.double],
+    Y: npt.NDArray[np.double],
+) -> npt.NDArray[np.double]:
+    """The update equation is:
+
+        X_posterior = X_prior + cov_XY @ inv(C_DD + alpha * C_D) @ (D - Y)
+
+    This function computes a matrix K such that
+
+        K := inv(C_DD + alpha * C_D) @ (D - Y)
+
+    Note that this is not the most efficient way to compute the update equation,
+    since cov_XY := center(X) @ center(Y) / (N - 1), and we can avoid creating
+    this (huge) matrix by performing multiplications left-to-right, i.e.,
+
+        cov_XY @ inv(C_DD + alpha * C_D) @ (D - Y)
+        center(X) @ center(Y) / (N - 1) @ inv(C_DD + alpha * C_D) @ (D - Y)
+        center(X) @ [center(Y) / (N - 1) @ inv(C_DD + alpha * C_D) @ (D - Y)]
+    """
+    C_DD = empirical_covariance_upper(Y)  # Only compute upper part
+
+    # Arguments for sp.linalg.solve
+    solver_kwargs = {
+        "overwrite_a": True,
+        "overwrite_b": True,
+        "assume_a": "pos",  # Assume positive definite matrix (use cholesky)
+        "lower": False,  # Only use the upper part while solving
+    }
+
+    # Compute K := sp.linalg.inv(C_DD + alpha * C_D) @ (D - Y)
+    if C_D.ndim == 2:
+        # C_D is a covariance matrix
+        C_DD += alpha * C_D  # Save memory by mutating
+    elif C_D.ndim == 1:
+        # C_D is an array, so add it to the diagonal without forming diag(C_D)
+        C_DD.flat[:: C_DD.shape[1] + 1] += alpha * C_D
+
+    return sp.linalg.solve(C_DD, D - Y, **solver_kwargs)
+
+
+# def empirical_cross_correlation(X, Y):
+#    co
+
+
 if __name__ == "__main__":
-    # Example showing how to use row scaling
-    num_parameters = 20
-    num_observations = 15
-    num_ensemble = 10
+
+    import time
+
+    # Create a problem
+    num_parameters = 200
+    num_observations = 150
+    num_ensemble = 25
 
     rng = np.random.default_rng(42)
 
@@ -36,34 +91,73 @@ if __name__ == "__main__":
     covariance = np.exp(rng.normal(size=num_observations))
     observations = rng.normal(size=num_observations, loc=1)
 
-    # Compute correlation matrix between parameters X and responses Y
-    from iterative_ensemble_smoother.esmda_inversion import empirical_cross_covariance
+    # Split the parameters into groups
+    split_index = rng.choice(range(num_parameters))
+    parameters_groups = [
+        tuple(range(split_index)),
+        tuple(range(split_index, num_parameters)),
+    ]
 
+    # Approach 1 - naive approach
+    # ----------------------------------------------------
+    start_time = time.perf_counter()
+    smoother = ESMDA(covariance=covariance, observations=observations, alpha=1, seed=1)
+    D = smoother.perturb_observations(size=Y.shape, alpha=1)
+
+    X_posterior = np.empty(shape=X.shape)
+
+    # Compute an update matrix, independent of X
+    K = compute_cross_covariance_multiplier(alpha=1, C_D=covariance, D=D, Y=Y)
+
+    stds_Y = np.std(Y, axis=1, ddof=1)
+    for parameter_idx in parameters_groups:
+        X_i = X[parameter_idx, :]
+
+        # Step 1: # Compute cross-correlation between parameters X and responses Y
+        # Note: let the number of parameters be n and the number of responses be m.
+        # This step requires both O(mn) computation and O(mn) storage, which is
+        # larger than the O(n + m^2) computation used in ESMDA, which never explicitly
+        # forms the cross-covariance matrix between X and Y
+        cov_XY = empirical_cross_covariance(X_i, Y)
+        stds_X = np.std(X_i, axis=1, ddof=1)
+        corr_XY = (cov_XY / stds_X[:, np.newaxis]) / stds_Y[np.newaxis, :]
+
+        # These will be set to zero
+        corr_XY_zero_mask = corr_XY < correlation_threshold(ensemble_size=X_i.shape[1])
+        cov_XY[corr_XY_zero_mask] = 0  # Set covariance to zero
+
+        X_posterior[parameter_idx, :] = X_i + cov_XY @ K
+
+    print(f"Approach 1 in {time.perf_counter() - start_time} s")
+
+    # Approach 2 - iterative approach
+    # ----------------------------------------------------
+    start_time = time.perf_counter()
+
+    # Step 1: # Compute cross-correlation matrix between parameters X and responses Y
+    # Note: let the number of parameters be n and the number of responses be m.
+    # This step requires both O(mn) computation and O(mn) storage, which is
+    # larger than the O(n + m^2) computation used in ESMDA, which never explicitly
+    # forms the cross-covariance matrix between X and Y
     cov_XY = empirical_cross_covariance(X, Y)
     stds_X = np.std(X, axis=1, ddof=1)
     stds_Y = np.std(Y, axis=1, ddof=1)
     corr_XY = (cov_XY / stds_X[:, np.newaxis]) / stds_Y[np.newaxis, :]
 
+    # These will be set to zero
     corr_XY_zero_mask = corr_XY < correlation_threshold(ensemble_size=X.shape[1])
-
-    # Approach 1 - naive approach
-    smoother = ESMDA(covariance=covariance, observations=observations, alpha=1, seed=1)
-    D = smoother.perturb_observations(size=Y.shape, alpha=1)
     cov_XY[corr_XY_zero_mask] = 0  # Set covariance to zero
-    C_DD = empirical_cross_covariance(Y, Y)
-    X_posterior = X + cov_XY @ sp.linalg.solve(C_DD + 1 * np.diag(covariance), (D - Y))
 
-    # Approach 2 - iterative approach
     smoother = ESMDA(covariance=covariance, observations=observations, alpha=1, seed=1)
     D = smoother.perturb_observations(size=Y.shape, alpha=1)
+    # Compute an update matrix, independent of X
+    K = compute_cross_covariance_multiplier(alpha=1, C_D=covariance, D=D, Y=Y)
     X_posterior2 = np.copy(X)
     for i in range(X.shape[0]):
         j_mask = ~corr_XY_zero_mask[i]
-        X_posterior2[i, :] += (
-            cov_XY[i, j_mask]
-            @ (sp.linalg.solve(C_DD + 1 * np.diag(covariance), (D - Y)))[j_mask, :]
-        )
+        X_posterior2[i, :] += cov_XY[i, j_mask] @ K[j_mask, :]
 
+    print(f"Approach 2 in {time.perf_counter() - start_time} s")
     assert np.allclose(X_posterior, X_posterior2)
 
 
