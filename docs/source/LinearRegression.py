40b1e0e340145a5546eb7bdc3014ac4772620c26
Blunde1 <lundeberent@gmail.com>
2024-01-03 13:04:09 +0100

    Copy linear_l1_regression from graphite-maps

--- docs/source/LinearRegression.py
+++ docs/source/LinearRegression.py
@@ -34,6 +34,9 @@
 # %%
 import numpy as np
 from matplotlib import pyplot as plt
+from sklearn.linear_model import LassoCV
+from sklearn.preprocessing import StandardScaler
+from tqdm import tqdm
 
 from iterative_ensemble_smoother import ESMDA
 
@@ -172,8 +175,70 @@ plt.grid(True, ls="--", zorder=0, alpha=0.33)
 plt.legend()
 plt.show()
 
+
 # %%
-from graphite_maps.linear_regression import linear_l1_regression
+def linear_l1_regression(U, Y):
+    """
+    Performs LASSO regression for each response in Y against predictors in U,
+    constructing a sparse matrix of regression coefficients.
+
+    The function scales features in U using standard scaling before applying
+    LASSO, then re-scales the coefficients to the original scale of U. This
+    extracts the effect of each feature in U on each response in Y, ignoring
+    intercepts and constant terms.
+
+    Parameters
+    ----------
+    U : np.ndarray
+        2D array of predictors with shape (n, p).
+    Y : np.ndarray
+        2D array of responses with shape (n, m).
+
+    Returns
+    -------
+    H_sparse : scipy.sparse.csc_matrix
+        Sparse matrix (m, p) with re-scaled LASSO regression coefficients for
+        each response in Y.
+
+    Raises
+    ------
+    AssertionError
+        If the number of samples in U and Y do not match, or if the shape of
+        H_sparse is not (m, p).
+    """
+    n, p = U.shape  # p: number of features
+    n_y, m = Y.shape  # m: number of y responses
+
+    # Assert that the first dimension of U and Y are the same
+    assert n == n_y, "Number of samples in U and Y must be the same"
+
+    scaler_u = StandardScaler()
+    U_scaled = scaler_u.fit_transform(U)
+
+    scaler_y = StandardScaler()
+    Y_scaled = scaler_y.fit_transform(Y)
+
+    # Loop over features
+    H = np.zeros((m, p))
+    for j in tqdm(range(m), desc="Learning sparse linear map for each response"):
+        y_j = Y_scaled[:, j]
+
+        # Learn individual regularization and fit
+        eps = 1e-3
+        max_iter = 10000
+        model_cv = LassoCV(cv=10, fit_intercept=False, max_iter=max_iter, eps=eps)
+        model_cv.fit(U_scaled, y_j)
+
+        # Extract coefficients
+        for non_zero_ind in model_cv.coef_.nonzero()[0]:
+            H[j, non_zero_ind] = (
+                scaler_y.scale_[j]
+                * model_cv.coef_[non_zero_ind]
+                / scaler_u.scale_[non_zero_ind]
+            )
+
+    return H
+
 
 # %%
 # Learn Kalman gain
