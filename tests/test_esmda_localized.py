88a72899063230af31543404258e4968f903bfc0
Tommy Odland <10076072+tommyod@users.noreply.github.com>
2026-02-16 10:50:17 +0000

    ESMDA vs. lESMDA on dense covariance (#288)
    * Started work on dense covariance
    * Test LocalizedESMDA vs. ESMDA on dense covar

--- tests/test_esmda_localized.py
+++ tests/test_esmda_localized.py
@@ -130,7 +130,8 @@ class TestLocalizedESMDA:
 
     @pytest.mark.parametrize("seed", range(99))
     @pytest.mark.parametrize("inversion", ["exact", "subspace"])
-    def test_equivalence_with_ESMDA(self, seed, inversion):
+    @pytest.mark.parametrize("dense_covariance", [True, False])
+    def test_equivalence_with_ESMDA(self, seed, inversion, dense_covariance):
         """With no localization, ESMDA and LocalizedESMDA should produce
         exactly the same results."""
 
@@ -162,7 +163,11 @@ class TestLocalizedESMDA:
 
         # Set up the localized ESMDA instance and the prior realizations X:
         covariance = np.logspace(-1, 1, num=num_obs)  # Covar of observations
-        covariance = np.ones(num_obs)  # Covar of observations
+
+        if dense_covariance:
+            factor = rng.normal(size=(num_obs, num_obs)) / num_obs
+            covariance = np.diag(covariance) + factor.T @ factor
+
         observations = np.zeros(num_obs)  # The observed data
         esmda = ESMDA(
             covariance=covariance,
@@ -173,7 +178,7 @@ class TestLocalizedESMDA:
         )
 
         lesmda = LocalizedESMDA(
-            covariance=np.diag(covariance),
+            covariance=covariance,
             observations=observations,
             alpha=alpha,
             seed=seed,  # Same seed
