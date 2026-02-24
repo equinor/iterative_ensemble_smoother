b1861a0537ec0dc0760fd06a4301b04f79a8d46b
Tommy Odland <tommy.odland@gmail.com>
2026-02-24 13:11:57 +0100

    Added function for masked std

--- src/iterative_ensemble_smoother/utils.py
+++ src/iterative_ensemble_smoother/utils.py
@@ -12,6 +12,57 @@ if TYPE_CHECKING:
 logger = logging.getLogger(__name__)
 
 
+def masked_std(X, *, missing):
+    """Computes a masked std for each row in X.
+
+    Examples
+    --------
+    >>> X = np.array([[ 0.65,  0.74,  0.54, -0.67],
+    ...               [ 0.23,  0.12,  0.22,  0.87],
+    ...               [ 0.22,  0.68,  0.07,  0.29]])
+
+    Let us encode some missing data:
+
+    >>> missing = np.array([[0, 0, 0, 0],
+    ...                     [0, 0, 0, 1],
+    ...                     [0, 0, 1, 1]], dtype=np.bool_)
+
+    For each row, the standard deviations are:
+
+
+    >>> for i in range(X.shape[0]):
+    ...     print(float(np.std(X[i, ~missing[i, :]], ddof=1)))
+    0.6617401302626281
+    0.060827625302982205
+    0.3252691193458119
+
+    This function computes the same standard deviations, but vectorized:
+
+    >>> masked_std(X, missing=missing)
+    array([0.66174013, 0.06082763, 0.32526912])
+    """
+
+    N_e = X.shape[1]  # Ensemble members / realizations
+    n_available = N_e - np.sum(missing, axis=1, keepdims=True)  # Non-missing params
+
+    # Need at least two observations per parameter
+    if np.any(n_available < 2):
+        msg = "One or several parameters have too few observations (need >=2)."
+        raise ValueError(msg)
+
+    X_masked = np.logical_not(missing) * X  # Set missing values to zero
+
+    # Compute mean values, taking missing into account
+    X_means = np.sum(X_masked, axis=1, keepdims=True) / n_available
+
+    # Center the matrix
+    X_centered = (X_masked - X_means) * np.logical_not(missing)
+
+    return np.sqrt(
+        np.sum(X_centered**2, axis=1, keepdims=True) / (n_available - 1)
+    ).ravel()
+
+
 def adjust_for_missing(
     X: npt.NDArray[np.double], *, missing: npt.NDArray[np.bool_]
 ) -> npt.NDArray[np.double]:
