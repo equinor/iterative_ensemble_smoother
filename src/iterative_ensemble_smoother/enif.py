19b083263a26c230cd8d3ba07ff2985c693f5c77
tommyod <tommy.odland>
2026-06-15 10:41:19 +0200

    ruff

--- src/iterative_ensemble_smoother/enif.py
+++ src/iterative_ensemble_smoother/enif.py
@@ -3,14 +3,14 @@ Ensemble Information Filter (EnIF)
 ----------------------------------
 
 The EnIF algorithm roughly consists of three main ideas:
-    
+
     1. Work with a form of the Kalman gain that requires (a) H, a linearization
        of the forward map Y = h(X) and (b) Prec(X), the prior parameter precision.
     2. Estimate H using sparse linear regression: Y = HX + r, where r are residuals
     3. Estimate Prec(X) using graph adjacency information if possible
-    
-The EnIF formulation estimates (H, Prec(X)) 
-while ESMDA estimates (Cov(X, Y), Cov(Y, Y)). 
+
+The EnIF formulation estimates (H, Prec(X))
+while ESMDA estimates (Cov(X, Y), Cov(Y, Y)).
 
 The advantage of estimating H and Prec(X) is that we can lean on high-dimensional
 regression algorithms (Lasso, linear boosting, etc.) to estimate H and
@@ -24,7 +24,8 @@ but EnIF class name was chosen because it is the appreviation already in use.
 References
 ----------
 
-- An Ensemble Information Filter: Retrieving Markov-information from the SPDE discretisation
+- An Ensemble Information Filter:
+  Retrieving Markov-information from the SPDE discretisation
   Berent Ånund Strømnes Lunde
   https://arxiv.org/abs/2501.09016
 
@@ -34,7 +35,7 @@ Examples
 >>> import numpy as np, scipy as sp
 >>> rng = np.random.default_rng(42)
 
-Create probelm size with parameters >> responses >> realizations.
+Create problem size with parameters >> responses >> realizations.
 
 >>> num_responses = 50
 >>> num_params = 100
@@ -42,15 +43,15 @@ Create probelm size with parameters >> responses >> realizations.
 >>> alpha = 3
 
 Create a true forward model:
-    
->>> H = sp.sparse.random_array(shape=(num_responses, num_params), 
+
+>>> H = sp.sparse.random_array(shape=(num_responses, num_params),
 ...                            density=0.1, rng=rng)
 >>> def forward(X):
 ...     linear = H @ X
 ...     return linear + 1e-3 * linear**2
 
 Diagonal observation (measurement) errors, prior ensemble, observations:
-    
+
 >>> covariance = np.logspace(-1, 1, num=num_responses)
 >>> X_prior = rng.normal(size=(num_params, num_realizations))
 >>> observations = forward(X_prior[:, 0] + 1).ravel()
@@ -62,19 +63,19 @@ estimate it from the prior.
 >>> parameter_precision = sp.sparse.diags_array(np.ones(num_params))
 
 Create the smoother:
-    
->>> enif = EnIF(covariance=covariance, observations=observations, 
-...             parameter_precision=parameter_precision, alpha=alpha, 
+
+>>> enif = EnIF(covariance=covariance, observations=observations,
+...             parameter_precision=parameter_precision, alpha=alpha,
 ...             seed=42, solver="cg")
 
 Assimilate data:
-    
+
 >>> X = np.copy(X_prior)
 >>> for _ in range(enif.num_assimilations()):
 ...
 ...     # Apply the forward model
 ...     Y = forward(X)
-...     enif.prepare_assimilation(Y=Y)     
+...     enif.prepare_assimilation(Y=Y)
 ...     residual_covariance = np.var(Y - H @ X, axis=1, ddof=1)
 ...     X = enif.assimilate(X=X, linearized_model=H,
 ...                         residual_covariance=residual_covariance)
@@ -83,11 +84,10 @@ Assimilate data:
 from typing import Union
 
 import numpy as np
-import scipy as sp
 import numpy.typing as npt
 
-from iterative_ensemble_smoother.esmda import BaseESMDA
 from iterative_ensemble_smoother.enif_utils import SPDSolver
+from iterative_ensemble_smoother.esmda import BaseESMDA
 
 
 class EnIF(BaseESMDA):
@@ -110,7 +110,7 @@ class EnIF(BaseESMDA):
     >>> covariance = np.ones(3, dtype=float)
     >>> observations = np.array([1, 2, 3], dtype=float)
     >>> parameter_precision = np.eye(10)
-    >>> smoother = EnIF(covariance=covariance, observations=observations, 
+    >>> smoother = EnIF(covariance=covariance, observations=observations,
     ...                 parameter_precision=np.eye(10), alpha=3, seed=42)
     >>> X = rng.normal(size=(10, 100))
 
@@ -128,7 +128,7 @@ class EnIF(BaseESMDA):
            [-0.8, -1.2,  0.5,  0.6, -1.1],
            [-0.3, -0.2,  1.2,  1.6, -0.4]])
     """
-    
+
     def __init__(
         self,
         *,
@@ -155,7 +155,6 @@ class EnIF(BaseESMDA):
             This is d_obs in Emerick (2013).
         parameter_precision: sp.sparse.sparray
             Sparse parameter precision of shape (num_parameters, num_parameters).
-            Can either be estimated from the data X, with shape (num_parameters, ensemble_size)
         alpha : int or 1D np.ndarray, optional
             Multiplicative factor for the covariance.
             If an integer `alpha` is given, an array with length `alpha` and
@@ -181,23 +180,24 @@ class EnIF(BaseESMDA):
         self.solver = solver
         self.solver_options = solver_options
         self.parameter_precision = parameter_precision
-        
+
         # Defaults for solvers
         if self.solver_options is None and self.solver == "cholesky":
             self.solver_options = {"ordering_method": "metis"}
         if self.solver_options is None and self.solver == "cg":
             self.solver_options = {"maxiter": 15}
-            
+
         # Creat solver for solving the main sym. pos. def. equation:
         # (Prec_x + H.T @ Prec @ H) X = H.T @ Prec @ (D - (Y + E))
-        self.spd_solver = SPDSolver(Prec_x=parameter_precision,
-                                    solver=self.solver,
-                                    solver_options=self.solver_options)
-        super().__init__(covariance=covariance, 
-                         observations=observations, 
-                         alpha=alpha, 
-                         seed=seed)
-    
+        self.spd_solver = SPDSolver(
+            Prec_x=parameter_precision,
+            solver=self.solver,
+            solver_options=self.solver_options,
+        )
+        super().__init__(
+            covariance=covariance, observations=observations, alpha=alpha, seed=seed
+        )
+
     def prepare_assimilation(
         self,
         *,
@@ -266,8 +266,7 @@ class EnIF(BaseESMDA):
         else:
             D_obs = self.perturb_observations(ensemble_size=N_e, alpha=alpha)
         self.D_obs_minus_D = D_obs - D
-        
-        
+
     def assimilate_batch(self, *args, **kwargs):
         # Override the base class method so users do not call it
         msg = "The EnIF class cannot assimilate batches\n Use .assimilate()"
@@ -290,11 +289,11 @@ class EnIF(BaseESMDA):
             to an ensemble member (realization).
         linearized_model : np.ndarray or None
             A 2D sparse array of shape (num_observations, num_parameters) that
-            represents a linearization of the forward model. Sometimes called H. 
+            represents a linearization of the forward model. Sometimes called H.
             Created by regressing Y on X, such that Y = H @ X.
         residual_covariance : np.ndarray or None
-            A 2D array of shape (num_observations, num_observations) that 
-            represents the covariance of the residuals of the linearized model. 
+            A 2D array of shape (num_observations, num_observations) that
+            represents the covariance of the residuals of the linearized model.
             If r = Y - H @ X, then the residual covariance is cov(r).
             If a 1D array is passed, the residual covariance is assumed linear,
             e.g. np.var(Y - H @ X, axis=1, ddof=1).
@@ -310,26 +309,32 @@ class EnIF(BaseESMDA):
         Cov_r = residual_covariance
         Cov_eps = self.alpha[self.iteration] * self.C_D
         innovation = self.D_obs_minus_D
-        
-        # Compute right-hand side of equation: 
+
+        # Compute right-hand side of equation:
         if Cov_r.ndim == 1 and Cov_eps.ndim == 1:
-            Prec_eps_r = (1/ (Cov_r + Cov_eps))
+            Prec_eps_r = 1 / (Cov_r + Cov_eps)
             RHS = (H.T * Prec_eps_r) @ innovation
         else:
             Cov_r = Cov_r if Cov_r.ndim == 2 else np.diag(Cov_r)
             Cov_eps = Cov_eps if Cov_eps.ndim == 2 else np.diag(Cov_eps)
             Prec_eps_r = np.linalg.inv(Cov_r + Cov_eps)
             RHS = H.T @ Prec_eps_r @ innovation
-        
+
         # Add terms to the left hand side
         self.spd_solver.add(H=H, Prec_eps_r=Prec_eps_r)
-        
+
         # Solve for change in X
         delta_X = self.spd_solver.solve(RHS)
         return X + delta_X
-        
-        
+
+
 if __name__ == "__main__":
     import pytest
 
-    pytest.main(args=[__file__,"--doctest-modules","-v",])
+    pytest.main(
+        args=[
+            __file__,
+            "--doctest-modules",
+            "-v",
+        ]
+    )
--- src/iterative_ensemble_smoother/enif_utils.py
+++ src/iterative_ensemble_smoother/enif_utils.py
@@ -1,34 +1,36 @@
+import logging
+
 import numpy as np
 import scipy as sp
-import logging
 
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
 
+
 class SPDSolver:
     """This class solves the equation:
-        
+
         (Prec_x + H.T @ Prec_eps_r @ H) X = (H.T @ Prec_eps_r @ innovation)
                                Prec_eps_r = inv(Cov_r + Cov_eps)
-                               
+
     The key observation is that the left-hand-side is sparse, pos.def and
     updated in every MDA-like iteration, in each iteration the LHS is:
-        
+
         1. (Prec_x + H_1.T @ Prec_eps_r_1 @ H_1)
         2. (Prec_x + H_1.T @ Prec_eps_r_1 @ H_1 + H_2.T @ Prec_eps_r_2 @ H_2)
         3. etc
-        
+
     Two ideas for solving this equation:
-        
+
         (a) Use conjugate gradients, but never explicitly form the LHS.
             Prefer many matvec operations over forming matrix (matmat).
             This is much faster, and saves storage.
         (b) Use Cholesky, but update the factor with a low-rank update
             instead of recomputing it in every MDA iteration.
-            
+
     On a problem with 5000 parameters, 2500 responses and 250 realizations:
         "dense" takes 2 seconds, "cholesky" 3 seconds and "cg" 17 seconds
-    
+
     Examples
     --------
     >>> import scipy as sp
@@ -36,21 +38,21 @@ class SPDSolver:
     >>> H = sp.sparse.csc_array([[1, 2, 3], [0, 2, 1]])
     >>> Prec_eps_r = np.array([0.1, 0.2])
     >>> b = sp.sparse.csc_array([[5, 1, 3], [4, 2, 3]]).T
-    
+
     >>> solver = SPDSolver(Prec_x=Prec_x, solver="dense")
     >>> solver.add(H=H, Prec_eps_r=Prec_eps_r)
     >>> solver.solve(b)
     array([[ 4.44615385,  3.47384615],
            [-0.1       ,  0.28      ],
            [ 0.43076923,  0.40923077]])
-    
+
     >>> solver = SPDSolver(Prec_x=Prec_x, solver="cg")
     >>> solver.add(H=H, Prec_eps_r=Prec_eps_r)
     >>> solver.solve(b)
     array([[ 4.44615385,  3.47384615],
            [-0.1       ,  0.28      ],
            [ 0.43076923,  0.40923077]])
-    
+
     >>> solver = SPDSolver(Prec_x=Prec_x, solver="cholesky")
     >>> solver.add(H=H, Prec_eps_r=Prec_eps_r)
     >>> solver.solve(b)
@@ -58,19 +60,20 @@ class SPDSolver:
            [-0.1       ,  0.28      ],
            [ 0.43076923,  0.40923077]])
     """
-    
+
     def __init__(self, *, Prec_x, solver="cg", solver_options=None):
         assert solver in ("dense", "cg", "cholesky")
         self.solver = solver
         self.solver_options = solver_options or dict()
-        
+
         Prec_x = sp.sparse.csc_array(Prec_x)
-        
+
         # Depending on the solver, we keep different state
         if self.solver == "cholesky":
-            from sksparse.cholmod import cholesky
+            from sksparse.cholmod import cholesky  # noqa: PLC0415
+
             # ordering_method: one of "natural", "amd", "metis",
-            # "nesdis", "colamd", "default" and "best". 
+            # "nesdis", "colamd", "default" and "best".
             # "natural" means no permutation.
             self.lhs = Prec_x.copy()
             self.lhs_factor = cholesky(self.lhs, **self.solver_options)
@@ -82,98 +85,91 @@ class SPDSolver:
             # while 'rest' refers to the low-rank updates
             self.first = Prec_x.copy()
             self.rest = []
-            
-            
-        
+
     def add(self, *, H, Prec_eps_r):
         """Add H.T @ Prec_eps_r @ H to the left-hand side of the equation."""
         if self.solver == "cholesky":
-            from sksparse.cholmod import cholesky
+            from sksparse.cholmod import cholesky  # noqa: PLC0415
+
             # Update the cholesky factorization of the LHS, adding C @ C.T.
             # Let L @ L.T = Prec_eps_r, then H.T @ Prec_eps_r @ H =
             # H.T @ L @ L.T @ H = (H.T @ L) @ (H.T @ L).T
             if Prec_eps_r.ndim == 1:
                 L = np.sqrt(Prec_eps_r)
-                C =sp.sparse.csc_array(H.T * L )
+                C = sp.sparse.csc_array(H.T * L)
             elif Prec_eps_r.ndim == 2 and isinstance(Prec_eps_r, np.ndarray):
                 L = np.linalg.cholesky(Prec_eps_r)
-                C = sp.sparse.csc_array(H.T @ L )
+                C = sp.sparse.csc_array(H.T @ L)
             elif Prec_eps_r.ndim == 2 and isinstance(Prec_eps_r, sp.sparse.sparray):
                 # TODO: Use sparse cholesky instead
                 L = np.linalg.cholesky(Prec_eps_r.todense())
-                C = sp.sparse.csc_array(H.T @ L )
+                C = sp.sparse.csc_array(H.T @ L)
             else:
                 raise ValueError("Unrecognized type")
-                
+
             # self.lhs.update_inplace(C =C)
             self.lhs += C @ C.T
             self.lhs_factor = cholesky(self.lhs, **self.solver_options)
-            
+
         # Store the matrices
         else:
             self.rest.append((H.copy(), Prec_eps_r.copy()))
-            
+
     def solve(self, b):
         """Solve (Prec_x + H.T @ Prec_eps_r @ H + ...) X = b for unknown X."""
 
         if self.solver == "dense":
             return self._solve_dense(b)
-        elif self.solver == "cg":
+        if self.solver == "cg":
             return self._solve_cg(b)
-        elif self.solver == "cholesky":
+        if self.solver == "cholesky":
             return self._solve_cholesky(b)
-        else:
-            raise ValueError(f"Unknown solver: {self.solver=}")
-        
-        
+        raise ValueError(f"Unknown solver: {self.solver=}")
+
     def _solve_dense(self, b):
         """Solve by forming dense matrices. Consumes a lot of memory, but
         is surprisingly fast for small problems."""
         LHS = self.first.copy()
-        for (H, Prec_eps_r) in self.rest:
+        for H, Prec_eps_r in self.rest:
             if Prec_eps_r.ndim == 1:
                 LHS += (H.T * Prec_eps_r) @ H
             else:
                 LHS += H.T @ Prec_eps_r @ H
-                
+
         # Densify input for the scipy cholesky solver
         LHS = LHS.todense() if isinstance(LHS, sp.sparse.sparray) else LHS
-        b = b.todense() if isinstance(b, sp.sparse.sparray) else b        
+        b = b.todense() if isinstance(b, sp.sparse.sparray) else b
         return sp.linalg.solve(LHS, b, assume_a="pos", overwrite_a=True)
-    
-    
+
     def _solve_cholesky(self, b):
         """Solve using sparse Cholesky factorization."""
         b = sp.sparse.csc_array(b)
         return sp.sparse.csc_array(self.lhs_factor.solve_A(b)).todense()
-        
-    
-    
+
     def _solve_cg(self, b):
-        """Solve using conjugate gradients on each column in the 
+        """Solve using conjugate gradients on each column in the
         right-hand-side b."""
         assert b.ndim == 2
         b = b.todense() if isinstance(b, sp.sparse.sparray) else b
-        
+
         def matvec_A(v):
             """Implement operator A @ v without forming A."""
-            result = self.first @ v 
-            for (H, Prec_eps_r) in self.rest:
+            result = self.first @ v
+            for H, Prec_eps_r in self.rest:
                 if Prec_eps_r.ndim == 1:
                     # Its important to apply product right-to-left
-                    result += ((H.T * Prec_eps_r) @ (H @ v))
+                    result += (H.T * Prec_eps_r) @ (H @ v)
                 else:
-                    result += (H.T @ (Prec_eps_r @ (H @ v)))
-                    
+                    result += H.T @ (Prec_eps_r @ (H @ v))
+
             return result
-        
-        
+
         def matvec_M(v):
             """Implement M ~= inv(A) without forming M."""
             # using diagonal only seems faster and better than e.g.
             # cholesky factoring Prec_x
             diag = self.first.diagonal().copy()
-            for (H, Prec_eps_r) in self.rest:
+            for H, Prec_eps_r in self.rest:
                 if Prec_eps_r.ndim == 1:
                     # Equivalent to: ((H.T * Prec_eps_r) @ H).diagonal()
                     diag += (Prec_eps_r[:, None] * H**2).sum(axis=0)
@@ -181,41 +177,43 @@ class SPDSolver:
                     # Equivalent to: ((H.T @ Prec_eps_r) @ H).diagonal()
                     diag += (H * (Prec_eps_r @ H)).sum(axis=0)
 
-            return v  / diag
-                    
-                    
+            return v / diag
+
         n_params, n_samples = self.first.shape[0], b.shape[1]
-        A = sp.sparse.linalg.LinearOperator(shape=(n_params, n_params),
-                                            matvec=matvec_A )
-        M = sp.sparse.linalg.LinearOperator(shape=(n_params, n_params),
-                                            matvec=matvec_M )
-                    
-            
-        X = np.zeros(shape=(n_params, n_samples)) # answer
+        A = sp.sparse.linalg.LinearOperator(shape=(n_params, n_params), matvec=matvec_A)
+        M = sp.sparse.linalg.LinearOperator(shape=(n_params, n_params), matvec=matvec_M)
+
+        X = np.zeros(shape=(n_params, n_samples))  # answer
         x0 = np.zeros(n_params)
-        cg_iterations = [] # Iteration counters per column of b
-        
+        cg_iterations = []  # Iteration counters per column of b
+
         for i in range(n_samples):
-            
             # Count number of iters to solve this column of b
             iters_i = 0
+
             def callback(xk):
                 nonlocal iters_i
                 iters_i = iters_i + 1
-            
-            # Relevant solver options: rtol=1e-05, atol=0.0, maxiter=None
-            x, info = sp.sparse.linalg.cg(A=A, b=b[:, i], M=M, callback=callback, x0=x0,
-                                          **self.solver_options)
-            X[:, i] = x
-            cg_iterations.append(iters_i) # Store iteration count
 
-        log.info(f"Ran CG with iters: mean={np.mean(cg_iterations):.1f} std={np.std(cg_iterations):.2f}")
+            # Relevant solver options: rtol=1e-05, atol=0.0, maxiter=None
+            x, info = sp.sparse.linalg.cg(
+                A=A, b=b[:, i], M=M, callback=callback, x0=x0, **self.solver_options
+            )
+            X[:, i] = x
+            cg_iterations.append(iters_i)  # Store iteration count
+
+        log.info("Ran conjugate gradients. Iterations per right-hand side:")
+        log.info(f"mean={np.mean(cg_iterations):.1f} std={np.std(cg_iterations):.2f}")
         return X
 
-    
+
 if __name__ == "__main__":
     import pytest
 
-    pytest.main(args=[__file__,"--doctest-modules","-v",])
-    
-    
+    pytest.main(
+        args=[
+            __file__,
+            "--doctest-modules",
+            "-v",
+        ]
+    )
